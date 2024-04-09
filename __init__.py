aca9ee100da15de9f6447230bfe5e2d5b3254895
Sohan Basak <ronnie.basak96@gmail.com>
2024-04-09 12:50:25 +0530

    Advanced Composition Node and PresetLoader

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from .py.tara_master import (
     TaraAPIKeyLoader,
     TaraDaisyChainNode,
 )
-from .py.tara_advanced import TaraPrompterAdvancedNode, TaraLLMConfigNode
+from .py.tara_advanced import TaraPrompterAdvancedNode, TaraLLMConfigNode, TaraAdvancedCompositionNode, TaraPresetLLMConfigNode
 # from .py.
 
 NODE_CLASS_MAPPINGS = {
@@ -14,15 +14,21 @@ NODE_CLASS_MAPPINGS = {
     "TaraDaisyChainNode": TaraDaisyChainNode,
     "TaraPrompterAdvanced": TaraPrompterAdvancedNode,
     "TaraLLMConfig": TaraLLMConfigNode,
-
+    "TaraPresetLLMConfig": TaraPresetLLMConfigNode,
+    "TaraAdvancedComposition": TaraAdvancedCompositionNode,
 }
+
+
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "TaraPrompter": "Tara LLM Primary Node",
+    "TaraPrompterAdvanced": "Tara Advanced LLM Node",
+    "TaraAdvancedComposition": "Tara Advanced LLM Composition Node",
     "TaraApiKeyLoader": "Tara LLM API Key Loader",
     "TaraApiKeySaver": "Tara LLM API Key Saver",
-    "TaraDaisyChainNode": "Tara LLM Daisy Chain Node",
-    "TaraPrompterAdvanced": "Tara LLM Advanced Node",
     "TaraLLMConfig": "Tara LLM Config Node",
+    "TaraPresetLLMConfig": "Tara Preset LLM Config Node",
+
+    "TaraPrompter": "(Deprecated) Tara LLM Primary Node",
+    "TaraDaisyChainNode": "(Deprecated) Tara LLM Daisy Chain Node (Deprecated)",
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
--- py/tara_advanced.py
+++ py/tara_advanced.py
@@ -3,7 +3,10 @@ import orjson
 import os.path
 import os
 from .tara_llm_config import TaraLLMConfig
-from .tara_master import get_llm_config, META_PROMPT, post_process_prompt
+from .tara_master import get_llm_config, META_PROMPT, post_process_prompt, get_model_names, MODEL_DICT
+
+
+
 
 
 class TaraPrompterAdvancedNode:
@@ -25,7 +28,8 @@ class TaraPrompterAdvancedNode:
             },
         }
 
-    def configure_chat_creation(self, llm_config, messages, overrides):
+    @staticmethod
+    def configure_chat_creation(llm_config, messages, overrides):
         config = get_llm_config(overrides=overrides)
         config["stream"] = False
 
@@ -81,6 +85,81 @@ class TaraPrompterAdvancedNode:
         )
 
 
+class TaraAdvancedCompositionNode:
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("output_text",)
+    FUNCTION = "generate_text"
+    CATEGORY = "tara-llm"
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "llm_config": ("TARA_LLM_CONFIG", {"forceInput": True}),
+                "guidance": ("STRING", {"multiline": True}),
+            },
+            "optional": {
+                "prompt": ("STRING", {"multiline": True, "forceInput": True}),
+                "positive": ("STRING", {"multiline": True, "forceInput": True}),
+                "negative": ("STRING", {"multiline": True, "forceInput": True}),
+            },
+        }
+
+    def generate_prompt(
+        self,
+        llm_config,
+        guidance,
+        prompt="",
+        positive="",
+        negative="",
+    ):
+        msgs = [
+            {
+                "role": "system",
+                "content": guidance,
+            },
+            {
+                "role": "user",
+                "content": f"""Provided Prompt: {prompt}""",
+            },
+        ]
+        if positive:
+            msgs.append(
+                {
+                    "role": "user",
+                    "content": f"Positive: {positive}",
+                }
+            )
+
+        if negative:
+            msgs.append(
+                {
+                    "role": "user",
+                    "content": f"Negative: {negative}",
+                }
+            )
+
+        overrides = {
+            "temperature": llm_config.temperature,
+            "max_tokens": llm_config.max_tokens,
+            "top_p": llm_config.top_p,
+            "frequency_penalty": llm_config.frequency_penalty,
+            "presence_penalty": llm_config.presence_penalty,
+            "response_format": {"type": "json_object"},
+            "seed": llm_config.seed,
+            "stream": False,
+            "timeout": llm_config.timeout,
+        }
+
+        data = TaraPrompterAdvancedNode.configure_chat_creation(
+            llm_config, msgs, overrides
+        )
+
+        return post_process_prompt(data["positive"]), post_process_prompt(
+            data["negative"]
+        )
+
+
 class TaraLLMConfigNode:
     RETURN_TYPES = ("TARA_LLM_CONFIG",)
     RETURN_NAMES = ("llm_config",)
@@ -129,5 +208,50 @@ class TaraLLMConfigNode:
                 frequency_penalty=frequency_penalty,
                 presence_penalty=presence_penalty,
                 timeout=timeout,
+            ),
+        )
+
+
+class TaraPresetLLMConfigNode:
+    RETURN_TYPES = ("TARA_LLM_CONFIG",)
+    RETURN_NAMES = ("llm_config",)
+    FUNCTION = "get_preset_llm_config"
+    CATEGORY = "tara-llm"
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "api_key": ("STRING", {"multiline": False, "forceInput": True}),
+                "llm_models": (get_model_names(MODEL_DICT), ),
+            }
+        }
+        
+    @staticmethod
+    def get_base_url_for_provider(provider):
+        if provider == "groq":
+            return "https://api.groq.com/openai/v1"
+        elif provider == "openai":
+            return "https://api.openai.com/v1"
+        else:
+            raise ValueError(f"Unsupported provider: {provider}")
+
+
+
+    def get_preset_llm_config(self, api_key, llm_models):
+        provider, llm_model_name = tuple(llm_models.split("/")[:2])
+        
+        return (
+            TaraLLMConfig(
+                base_url=self.get_base_url_for_provider(provider),
+                api_key=api_key,
+                llm_model=llm_model_name,
+                temperature=0.4,
+                seed=42,
+                max_tokens=1024,
+                top_p=1.0,
+                frequency_penalty=0.0,
+                presence_penalty=0.0,
+                timeout=60,
             ),
         )
